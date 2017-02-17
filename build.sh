890ca7d48cc8c8698006475b106d9192325993a9
thattommyhall <thattommyhall@gmail.com>
2017-02-17 17:05:37 +0000

    Switch index for metic entries

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DATE=`date --iso-8601`
 
-for IMAGE_NAME in logstash-ng awscli
+for IMAGE_NAME in logstash-ng
 do
   # --no-cache
   docker build -t mastodonc/$IMAGE_NAME:$DATE ./$IMAGE_NAME
