96ffa8bc45f7c69eb750abab64029d2a3f9ffaec
Matt Ford <matt@dancingfrog.co.uk>
2017-08-02 16:28:26 +0100

    Experiment

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DATE=`date +%Y-%m-%d`
-for IMAGE_NAME in logstash-ng database-metricbeat
+for IMAGE_NAME in logstash-ng 
 do
   docker build -t mastodonc/$IMAGE_NAME:$DATE ./$IMAGE_NAME
   docker push mastodonc/$IMAGE_NAME:$DATE
